f6ac44cdfd159005a6b39a2b9cd3dc8d00b8ebca
kstejas2001 <kstejas2001@gmail.com>
2026-03-19 17:47:33 +0530

    feat(day-01): add first variable and addition examples in python cpp and java

---
+++ day-01-how-computer-stores-data/problem_01_add_two_numbers.java
@@ -0,0 +1,17 @@
+// =============================================
+// File     : problem_01_add_two_numbers.java
+// Day      : 01 — How a Computer Stores Data
+// Topic    : Variables & Data Types
+// =============================================
+
+public class problem_01_add_two_numbers {
+
+    public static void main(String[] args) {
+
+        int a = 5;        // store 5
+        int b = 3;        // store 3
+        int c = a + b;    // add and store
+
+        System.out.println(c);  // print result
+    }
+}
