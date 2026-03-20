f7a1c5becb9a033b8c407058fc18c5f02b30da3b
kstejas2001 <kstejas2001@gmail.com>
2026-03-20 23:12:56 +0530

    feat(day-02/python): add user input addition program

---
+++ day-02-user-input-and-variables/problem-01_add-two-numbers-input.py
@@ -0,0 +1,21 @@
+# =============================================
+# File     : problem-01_add-two-numbers-input.py
+# Day      : 02 — User Input & Variables
+# Topic    : Taking input and adding two numbers
+# =============================================
+
+# Ask the user to enter first number
+a = input("Enter first number: ")
+
+# Ask the user to enter second number
+b = input("Enter second number: ")
+
+# Convert text input into integers
+a = int(a)
+b = int(b)
+
+# Add both numbers
+c = a + b
+
+# Show the result
+print(c)
