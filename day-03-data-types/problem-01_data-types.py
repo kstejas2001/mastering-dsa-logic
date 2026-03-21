1a9999f6a6ed0424e33120f1838e19954ef59275
kstejas2001 <kstejas2001@gmail.com>
2026-03-21 23:27:02 +0530

    feat(day-03/python): add basic data types example

---
+++ day-03-data-types/problem-01_data-types.py
@@ -0,0 +1,15 @@
+# =============================================
+# File     : problem-01_data-types.py
+# Day      : 03 — Data Types
+# Topic    : Basic data types in Python
+# =============================================
+
+age = 20
+price = 99.5
+name = "Crazy"
+is_student = True
+
+print(age)
+print(price)
+print(name)
+print(is_student)
