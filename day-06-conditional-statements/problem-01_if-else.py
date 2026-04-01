10dad2dd610a34d11b372b78cfecbc1940161759
kstejas2001 <kstejas2001@gmail.com>
2026-04-01 22:52:18 +0530

    feat(day-06/python): add if-else example

---
+++ day-06-conditional-statements/problem-01_if-else.py
@@ -0,0 +1,12 @@
+# =============================================
+# File     : problem-01_if-else.py
+# Day      : 06 — Conditional Statements
+# Topic    : if and else in Python
+# =============================================
+
+age = 18
+
+if age >= 18:
+    print("You are an adult")
+else:
+    print("You are a minor")
