82429cb4219fee6a0c74c2bcc1177e3b8f066709
kstejas2001 <kstejas2001@gmail.com>
2026-04-01 22:52:59 +0530

    feat(day-06/cpp): add if-else example

---
+++ day-06-conditional-statements/problem-01_if-else.cpp
@@ -0,0 +1,20 @@
+// =============================================
+// File     : problem-01_if-else.cpp
+// Day      : 06 — Conditional Statements
+// Topic    : if and else in C++
+// =============================================
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    int age = 18;
+
+    if (age >= 18) {
+        cout << "You are an adult" << endl;
+    } else {
+        cout << "You are a minor" << endl;
+    }
+
+    return 0;
+}
